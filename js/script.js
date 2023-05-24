ff773120704cf81c304b81f1a0e7e369ea092ee4
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2023-05-24 22:25:27 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // ДОБАВЛЕНИЕ НОМЕРА ПРОИЗВЕДЕНИЯ ПО ОЧЕРЕДИ
 const arrayOfCompositions = document.querySelectorAll('.programm__point');
 arrayOfCompositions.forEach((el, index) => {
-	el.innerHTML = (index + 1.) + '. ' + el.innerHTML;
+	el.innerHTML = (index + 1) + '. ' + el.innerHTML;
 })
 
 //АНИМАЦИЯ ПРИ СКРОЛЛЕ
